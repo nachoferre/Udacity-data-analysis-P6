a85316b4e1409e7717d25c5e990f6fcabf8d2510
nachoferre <nachoferreras93@gmail.com>
2017-03-29 23:20:00 +0000

    sample added

--- cleanup.py
+++ cleanup.py
@@ -1,7 +1,10 @@
 import json
+import random
 
-with open("Data/1987_sample.json") as fd:
+
+with open("Data/1987_clean.json") as fd:
 	data = json.load(fd)
+	output = random.sample(data, 100000)
 	# output = data[:10000]
 	# for elem in data:
 	# 	elem["origin_lat"] = float(elem["origin_lat"])
@@ -10,7 +13,6 @@ with open("Data/1987_sample.json") as fd:
 	# 	elem["dest_lat"] = float(elem["dest_lat"])
 	# 	elem["dest_long"] = float(elem["dest_long"])
 
-	# with open("Data/1987_sample"+".json", 'w') as fd:
-	#	json.dump(output, fd)
-	for elem in data:
-		print data["origin_long"]
+	with open("Data/1987_sample"+".json", 'w') as fd:
+		json.dump(output, fd)
+	
