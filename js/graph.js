8977d0607684ac3cad0651872258ed6d7f00bc6f
nachoferre <nachoferreras93@gmail.com>
2017-03-29 20:34:20 +0000

    points added

--- js/graph.js
+++ js/graph.js
@@ -27,13 +27,23 @@ function draw(geo_data) {
                      .style('stroke', 'black')
                      .style('stroke-width', 0.5);
         function flights(data) {
+          function update(leaves){
+              var coords = leaves.map(function(d) {
+                    return projection([d["origin_long"], d["origin_lat"]]);
+                });
+              debugger;
+              return coords
+          }
+          var data_update = d3.nest()
+                              .rollup(update)
+                              .entries(data);
           debugger;
           svg.selectAll("circle")
-             .data(data)
+             .data(data_update)
              .enter()
              .append("circle")
-             .attr("cx", function(d){return d["origin_lat"]})
-             .attr("cy", function(d){return d["origin_long"]})
+             .attr("cx", function(d){return d[0]})
+             .attr("cy", function(d){return d[1]})
              .attr("r", "8px")
              .attr("fill", "red");
         };
