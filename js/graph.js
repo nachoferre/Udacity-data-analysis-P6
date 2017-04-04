b265267d82f81db568dd3ea88870d1d34fd304ac
nachoferre <nachoferreras93@gmail.com>
2017-04-04 23:43:29 +0000

    working map

--- js/graph.js
+++ js/graph.js
@@ -3,13 +3,12 @@ function draw(geo_data) {
         var margin = 75,
             width = 1000 - margin,
             height = 600 - margin;
-        var pi = Math.PI,
-            tau = 2 * pi;
+        var graticule = d3.geo.graticule()
         var svg = d3.select("body")
             .append("svg")
             .attr("width", width + margin)
             .attr("height", height + margin);
-
+            
         var projection = d3.geo.mercator()
                                .scale(140)
                                .translate([width / 2, height / 1.2]);
@@ -35,37 +34,14 @@ function draw(geo_data) {
           return [d["origin_long"], d["origin_lat"]]
         }))
 
-        var lineTransition = function lineTransition(path) {
-            path.transition()
-                //NOTE: Change this number (in ms) to make lines draw faster or slower
-                .duration(5500)
-                .attrTween("stroke-dasharray", tweenDash)
-                .each("end", function(d,i) { 
-                    ////Uncomment following line to re-transition
-                    //d3.select(this).call(transition); 
-                    
-                    //We might want to do stuff when the line reaches the target,
-                    //  like start the pulsating or add a new point or tell the
-                    //  NSA to listen to this guy's phone calls
-                    //doStuffWhenLineFinishes(d,i);
-                });
-        };
-        var tweenDash = function tweenDash() {
-            //This function is used to animate the dash-array property, which is a
-            //  nice hack that gives us animation along some arbitrary path (in this
-            //  case, makes it look like a line is being drawn from point A to B)
-            var len = this.getTotalLength(),
-                interpolate = d3.interpolateString("0," + len, len + "," + len);
-
-            return function(t) { return interpolate(t); };
-        };
+        
         var origin = data.map(function(d) {
-                    return projection([+d["origin_long"], +d["origin_lat"]]);
+                    return projection([d["origin_long"], d["origin_lat"]]);
                 });
         var destination = data.map(function(d) {
-                    return projection([+d["dest_long"], +d["dest_lat"]]);
+                    return projection([d["dest_long"], d["dest_lat"]]);
                 });
-        debugger;
+
         var links = [];
         for(var i=0, len=data.length-1; i<len; i++){
             // (note: loop until length - 1 since we're getting the next
@@ -73,23 +49,43 @@ function draw(geo_data) {
             links.push({
                 type: "LineString",
                 coordinates: [
-                    [ origin[i][0], origin[i][1]],
-                    [ destination[i][0], destination[i][1] ]
+                    [data[i][["origin_long"]],data[i][["origin_lat"]]],
+                    [data[i]["dest_long"],data[i]["dest_lat"]]
                 ]
             });
         }
         debugger;
-        var pathArcs = svg.append('g')
-                          .selectAll('.arc')
-                          .data(links)
-                          .enter()
-                          .append('path')
-                          .attr('d', path)
-                          .style({
-                              stroke: '#0000ff',
-                              'stroke-width': '2px'})
-                          .call(lineTransition)
+        console.log(links)
+        svg.append('g')
+           .selectAll('path')
+           .data(links)
+           .enter()
+           .append('path')
+           .attr('d', path)
+           .style({
+              stroke: 'grey',
+              'stroke-width': '1px'})
+        
         debugger;
+        svg.append('g')
+               .attr("class", "bubble")
+               .selectAll("circle")
+               .data(links.slice(1, 2))
+               .enter()
+               .append("circle")
+               .attr('cx', function(d) { return d.coordinates[0][0]; })
+               .attr('cy', function(d) { return d.coordinates[0][1]; })
+               .attr('r', "2px");
+        svg.append('g')
+               .attr("class", "bubble")
+               .selectAll("circle")
+               .data(links.slice(1, 2))
+               .enter()
+               .append("circle")
+               .attr('cx', function(d) { return d.coordinates[1][0]; })
+               .attr('cy', function(d) { return d.coordinates[1][1]; })
+               .attr('r', "2px")
+               .style('fill', 'lightred')
                    
       })
     };
